2c2ba3b9ed7e13b4c920fec286543b3133cb709a
MJL7068 <mattil@netti.fi>
2019-02-28 15:42:38 +0200

    6.18 and 6.19 done

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const addAnecdote = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    const newAnecdote = anecdoteService.createNew(content)
-    props.createAnecdote(newAnecdote)
-    props.setNotification(`you created '${newAnecdote.content}'`)
+    props.createAnecdote(content)
+    props.setNotification(`you created '${content}'`)
     setTimeout(() => {
       props.setNotification(null)
     }, 5000)
@@ -28,18 +26,10 @@ const AnecdoteForm = (props) => {
   )
 }
 
-const mapStateToProps = (state) => {
-  return {
-
-  }
-}
-
-const mapDispatchToProps = {
-  createAnecdote,
-  setNotification
-}
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  null,
+  {
+    createAnecdote,
+    setNotification
+  }
 )(AnecdoteForm)
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+import anecdoteService from '../services/anecdotes'
 
 const reducer = (state = [], action) => {
   switch (action.type) {
@@ -30,7 +11,7 @@ const reducer = (state = [], action) => {
       }
       return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote).sort((a, b) => b.votes - a.votes)
     case 'NEW_ANECDOTE':
-      return [...state, asObject(action.data.content)]
+      return [...state, action.data]
     case 'INIT_ANECDOTES':
       return action.data
     default:
@@ -39,19 +20,23 @@ const reducer = (state = [], action) => {
 }
 
 export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
+  
 }
 
-export const createAnecdote = (anecdote) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: {
-      content: anecdote.content,
-      votes: anecdote.votes
-    }
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote,
+    })
   }
 }
 
